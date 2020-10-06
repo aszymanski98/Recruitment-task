12f2f563e3ae931e14399bd2d0883e2cbae0356a
aszymanski98 <adam.szymanski@protonmail.com>
2020-10-06 12:16:01 +0200

    add mobile styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const getCurrentRotation = (el) => {
-  var st = window.getComputedStyle(el, null);
-  var tm = st.getPropertyValue("-webkit-transform") || st.getPropertyValue("-moz-transform") || st.getPropertyValue("-ms-transform") || st.getPropertyValue("-o-transform") || st.getPropertyValue("transform") || "none";
+const getCurrentDeg = (el) => {
+  let st = window.getComputedStyle(el, null);
+  let tm = st.getPropertyValue("-webkit-transform") || st.getPropertyValue("-moz-transform") || st.getPropertyValue("-ms-transform") || st.getPropertyValue("-o-transform") || st.getPropertyValue("transform") || "none";
   if (tm != "none") {
     let values = tm.split("(")[1].split(")")[0].split(",");
     let angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
@@ -11,13 +11,13 @@ const getCurrentRotation = (el) => {
 
 let buttonRight90 = document.getElementById("rotate-right90").addEventListener("click", () => {
   let dog = document.querySelector(".dog-img");
-  let deg = getCurrentRotation(document.querySelector(".dog-img"));
+  let deg = getCurrentDeg(document.querySelector(".dog-img"));
   dog.style.transform = `rotate(${deg + 90}deg)`;
 });
 
 let buttonLeft90 = document.getElementById("rotate-left90").addEventListener("click", () => {
   let dog = document.querySelector(".dog-img");
-  let deg = getCurrentRotation(document.querySelector(".dog-img"));
+  let deg = getCurrentDeg(document.querySelector(".dog-img"));
   dog.style.transform = `rotate(${deg - 90}deg)`;
 });
 
@@ -29,17 +29,15 @@ let buttonReset = document.getElementById("rotate-reset").addEventListener("clic
 let buttonRight = document.getElementById("rotate-right-custom").addEventListener("click", () => {
   let dog = document.querySelector(".dog-img");
   let input = document.querySelector(".degs");
-  let deg = getCurrentRotation(document.querySelector(".dog-img"));
+  let deg = getCurrentDeg(document.querySelector(".dog-img"));
   let degFromInput = input.value;
-  console.log(deg);
-  console.log(degFromInput);
   dog.style.transform = `rotate(${deg + Number(degFromInput)}deg)`;
 });
 
 let buttonLeft = document.getElementById("rotate-left-custom").addEventListener("click", () => {
   let dog = document.querySelector(".dog-img");
   let input = document.querySelector(".degs");
-  let deg = getCurrentRotation(document.querySelector(".dog-img"));
+  let deg = getCurrentDeg(document.querySelector(".dog-img"));
   let degFromInput = input.value;
   dog.style.transform = `rotate(${deg - degFromInput}deg)`;
 });
